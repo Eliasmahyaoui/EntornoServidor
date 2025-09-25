efd52ce2c9b9f40b548698fd97702216b3c800f4
Elias Mahyaoui Fellah <eliasmahyaoui6@hotmail.com>
2025-09-26 00:58:40 +0200

    26/09/2025 -  0:58

--- cardsElias/blackjackElias.php
+++ cardsElias/blackjackElias.php
@@ -3,13 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/styles.css">
     <title>Black Jack</title>
 </head>
 <body>
     
     <h1>Black Jack Elias</h1>  
      
-     /* Incluye el header */  
+     
     <?php
     require_once($_SERVER['DOCUMENT_ROOT'] . '/includes/header.inc.php');
     ?>
--- cardsElias/higherElias.php
+++ cardsElias/higherElias.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/styles.css">
     <title>Higher</title>
 </head>
 <body>
@@ -16,20 +17,25 @@
 
     /*Aqui le pasamos la funcioon de getDesk */
      require_once($_SERVER['DOCUMENT_ROOT'] . '/includes/getDeck.inc.php');
+
     /*Tendremos que crear la variable y dentro de la variable darle la funcion */
     $deck=generateFrenchDeck();
+
     /*Con la funcion shufle lo podnra aleatorio*/ 
     shuffle($deck);
+
     /*Esto es puntual para mostrarlo por ahora*/
     //echo var_dump($deck);
 
     //Se repartirá 10 cartas a cada jugador (creamos los jugadores)
     
     for ($i=0; $i <=10 ; $i++) { 
+
         //Aqui lo que hacemos es coger una carta de la baraja.
         $hand1[] = array_pop($deck);    
         $hand2[] =array_pop($deck); 
     }
+
     //ahora lo que hacemos es darla nombres a los dos jugaodres
     $player1= "Harry";
     $player2="Voldemord";
@@ -42,14 +48,23 @@
         // echo $card['value'] .'-'. $card['suit'];
         // echo $card['image'];
         echo '<img src="/images/baraja/'.$card['image'].'" alt="card">';
-        echo '<br>';
+      
+    }
+
+     echo '<h2>'. $player2 .'</h2>';
+
+    foreach ($hand2 as $card) {
+        // echo $card['value'] .'-'. $card['suit'];
+        // echo $card['image'];
+        echo '<img src="/images/baraja/'.$card['image'].'" alt="card">';
+        
     }
     
 
 
     ?>
 
-    <img src="" alt="">
+    
  <?php
 
 
