44bc85f4f283967b2f483b729de21608e2b07c74
Elias Mahyaoui Fellah <eliasmahyaoui6@hotmail.com>
2025-09-29 23:58:05 +0200

    29/09/2025 - 23:58

--- cardsElias/blackjackElias.php
+++ cardsElias/blackjackElias.php
@@ -19,58 +19,109 @@
 
     <div id="contenido-principal">
         <?php
-        /*Aqui metemos la funcion que nos han dado */
+        /* Aquí metemos la función que nos han dado */
         require_once($_SERVER['DOCUMENT_ROOT'] . '/includes/getDeck.inc.php');
 
-        /*guardamos el array de cartas en deck. Array de cartas viene de la funcion generada con IA*/
-        $deck = generateFrenchDeck();
+        /* Generamos el mazo */
+        $deckRaw = generateFrenchDeck();
 
-        /*Esta funcion/método array_pop lo que hace es coger la ultima posicion de la carta*/
-        array_pop($deck);
+        /* Transformamos el mazo para que tenga 'name', 'value' numérico y 'image' */
+        $deck = [];
+        foreach ($deckRaw as $card) {
+            $name = $card['value'];
+            if ($name === 'J' || $name === 'Q' || $name === 'K') {
+                $value = 10;
+            } elseif ($name === '1') { // As
+                $name = 'AS';
+                $value = 11;
+            } else {
+                $value = (int)$name;
+            }
+            $deck[] = [
+                'name' => $name,
+                'value' => $value,
+                'image' => $card['image']
+            ];
+        }
 
-        /*Ahora queremos que se ordene aleaotoriamente el array de cartas, con la funcion shuffle*/
+        /* Mezclamos las cartas */
         shuffle($deck);
 
-        /* El juego dispondra de 5 jugadores y la banca (6 en total)*/
-        $players= ["Jordi", "Dembele", "Dani", "Alex", "Elias", "Banca"];
+        /* Definimos jugadores */
+        $players = ["Jordi", "Dembele", "Dani", "Alex", "Elias", "Banca"];
 
-        /*Se le repartirá dos cartas a cada jugadorextrayéndolas del array funciones: shift_pop o
-        array_shift) alternativamente (una a cada jugador cada vez).*/
-
-        $hands=[];
-
-        foreach ($players as $player) {
-            $hands[$player]= [];
+        /* Repartimos dos cartas a cada jugador */
+        $hands = [];
+        for ($i = 0; $i < 2; $i++) {
+            for ($j = 0; $j < count($players); $j++) {
+                $hands[$players[$j]][] = array_pop($deck);
+            }
         }
-        
-        foreach ($players as $player) {
-            $hands[$player][]= array_pop($deck);
-            $hands[$player][]= array_pop($deck);
-            
-        
-        }
-            
 
+        /* Función para calcular puntos, As puede valer 1 o 11 */
+        function calculatePoints($cards)
+        {
+            $sum = 0;
+            $aces = 0;
+
+            foreach ($cards as $card) {
+                $sum += $card['value'];
+                if ($card['name'] === 'AS') {
+                    $aces++;
+                }
+            }
+
+            while ($sum > 21 && $aces > 0) {
+                $sum -= 10; // Contar As como 1
+                $aces--;
+            }
+
+            return $sum;
+        }
+
+        /* Repartir cartas extra mientras puntos < 14 */
+        foreach ($players as $player) {
+            while (calculatePoints($hands[$player]) < 14 && count($deck) > 0) {
+                $hands[$player][] = array_pop($deck);
+            }
+        }
+
+        /* Calcular puntos finales */
+        $points = [];
+        foreach ($players as $player) {
+            $points[$player] = calculatePoints($hands[$player]);
+        }
+
+        /* Comparar jugadores con la banca */
+        $bancaPoints = $points['Banca'];
+        foreach ($players as $player) {
+            if ($player === 'Banca') continue;
+
+            echo $player . " tiene " . $points[$player] . " puntos. ";
+
+            if ($points[$player] > 21) {
+                echo "Ha perdido.<br>";
+            } elseif ($points[$player] > $bancaPoints || $bancaPoints > 21) {
+                echo "Ha ganado.<br>";
+            } elseif ($points[$player] < $bancaPoints) {
+                echo "Ha perdido.<br>";
+            } else {
+                echo "Empate.<br>";
+            }
+        }
+
+        /* Mostrar cartas de cada jugador como imágenes */
+        foreach ($hands as $playerName => $cards) {
+            echo "<strong>$playerName:</strong> ";
+            foreach ($cards as $card) {
+                echo '<img src="/images/baraja/' . $card['image'] . '" alt="card" style="width:50px;">';
+            }
+            echo "<br>";
+        }
         ?>
-
-
-
     </div>
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-    <div id=footer-principal>
+    <div id="footer-principal">
         <?php
         require_once($_SERVER['DOCUMENT_ROOT'] . '/includes/footer.inc.php');
         ?>
