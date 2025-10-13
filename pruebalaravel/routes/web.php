641c3e6f77047be8629d872945d73448b5d29471
elias <eliasmahyaoui6@hotmail.com>
2025-10-13 21:23:42 +0200

    13/10/2025 - 21:23

--- pruebalaravel/routes/web.php
+++ pruebalaravel/routes/web.php
@@ -3,5 +3,31 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    //return view('welcome');
+    return 'proyecto Laravel Elias <br><a href="'.route('saludo',['name'=>'Rick']). ' ">Rick</a>';
 });
+
+Route::get('datetime', function () {
+    return date('d/m/y h:i:s');
+});
+
+Route::get('juegos/higuercard', function () {
+    return 'aqui se mostrara las cartas de higuer';
+});
+
+Route::get('juegos/blackjack', function () {
+    return 'aqui se mostrara las cartas de blackjack';
+});
+
+Route::get('juegos/donde-estamos', function () {
+    return 'descrpicion del sitio ';
+});
+
+Route::get('greet/{name}', function (string $name) {
+    return 'Hola' . $name;
+})->name('saludo');
+
+
+Route::redirect('date','datetime');
+
+
