895e7f2772dd2999023615e96b0e900f46af9cb9
Eliasmahyaoui <eliasmahyaoui6@hotmail.com>
2025-10-23 03:16:59 +0200

    actualizado

---
+++ gestionLibrosPOO/Includes/index.php
@@ -0,0 +1,23 @@
+<?php
+require_once($_SERVER['DOCUMENT_ROOT' . '/Includes/autor.inc.php']);
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+
+    $autor1= new Autor("Cervantes","cervante@gmail.com");
+    
+
+    ?>
+
+</body>
+
+</html>
