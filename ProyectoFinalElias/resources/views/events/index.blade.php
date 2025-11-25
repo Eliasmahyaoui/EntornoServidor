30a6d0e9f3719dd7a90734e7975e2e09ea23c9b1
Eliasmahyaoui <eliasmahyaoui6@hotmail.com>
2025-11-25 15:19:23 +0100

    25/11/2025 - 15:19

--- ProyectoFinalElias/resources/views/events/index.blade.php
+++ ProyectoFinalElias/resources/views/events/index.blade.php
@@ -5,8 +5,15 @@ Eventos
 @endsection
 
 @section('content')
-    <h1>Proximos eventos</h1>
+    <h1>Mostrara una lista de próximos eventos</h1>
+    <ul>
+        <li>Próximo partido Alcaraz- Sinner</li>
+        <li>Próximo partido Barbosa - Nadal</li>
+        <li>Próximo partido Sara - Cristina</li>
+        <li>Próximos partidos...</li>
+    </ul>
 
-    
+
+
 @endsection
 
--- ProyectoFinalElias/resources/views/players/index.blade.php
+++ ProyectoFinalElias/resources/views/players/index.blade.php
@@ -1,19 +1,23 @@
 @extends('layout.layout');
 
 @section('title')
-Players
+    Players
 @endsection
 
 
 @section('content')
-    <h1>Mostrara una lista de próximos eventos</h1>
-    <ul>
-        <li>Próximo partido Alcaraz- Sinner</li>
-        <li>Próximo partido Barbosa - Nadal</li>
-        <li>Próximo partido Sara - Cristina</li>
-        <li>Próximos partidos...</li>
-    </ul>
+    <h1>Jugadores Masculinos:</h1>
 
+    <div id="cards">
+        
+        <img src="{{ asset('imagenes/Carlos_Alcaraz_card (1).png') }}" alt="Carlos Alcaraz">
+        <img src="{{ asset('imagenes/Rafa_Nadal_card (1).png') }}" alt="Rafa Nadal">
+        <img src="{{ asset('imagenes/Alejandro_Davidovivh_card.png') }}" alt="Alejandro_Davidovich">
+        <img src="{{ asset('imagenes/Roberto_Bautista_card.png') }}" alt="Roberto Bautista">
+
+        <h1>Jugadoras Femeninas:</h1>
+        <img src="{{ asset('imagenes/Sara_Sorribes_card.png') }}" alt="Sara Sorribes">
+        <img src="{{ asset('imagenes/Cristina_Bucsa_card.png') }}" alt="Cristina Bucsa">
+        <img src="{{ asset('imagenes/Paula_Badosa_card.png') }}" alt="Paula Badosa">
+    </div>
 @endsection
-
-
