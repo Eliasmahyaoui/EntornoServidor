bc242cd78205ea306bef95ca73f890cffe94c2db
Eliasmahyaoui <eliasmahyaoui6@hotmail.com>
2025-12-03 02:57:26 +0100

    03/12/2025 -  2:57

--- fluxvidelias/resources/views/characters/create.blade.php
+++ fluxvidelias/resources/views/characters/create.blade.php
@@ -21,28 +21,5 @@
     <br>
     <input type="submit" value="Guardar">
 </form>
-=======
-@extends('layout.layout')
 
 
-@sect
-<form action="{{route('characters.store')}}" method="POST">
-     @csrf
-
-    Nombre:<input type="text" name="name">
-    Edad:<input type="text" name="age">
-    Genero:<input type="text" name="gender">
-    Rol:<input type="text" name="roles">
-    Descripcion<input type="text" name="description">
-    Guardar<input type="submit" value="Guardar">
-
-
-</form>
-
-
-
-
-
-
-
->>>>>>> ecbba96684e66dfc55ba58741a91e924fdf99f89
